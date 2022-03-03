31da9a9ea2a2e7c54ef92f2cf3b12048aba7e36f
Ilya547 <ilya547@bk.ru>
2022-03-03 10:38:58 +0300

    remove Main.java

--- Lesson 1/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        int sum = 1 + 2 + 3 + 4 + 5;
-        String str = "Hello Word";
-        System.out.println(str);
-    }
-}
