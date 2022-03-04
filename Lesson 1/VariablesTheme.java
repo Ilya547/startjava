07b531e4cbb3e5c6de3fdca9dee2f4c58b613dcc
Ilya547 <ilya547@bk.ru>
2022-03-04 15:43:18 +0300

    2 update HW#1

--- Lesson 1/VariablesTheme.java
+++ Lesson 1/VariablesTheme.java
@@ -13,15 +13,13 @@ public class VariablesTheme {
         System.out.println("numCores = " + numCores + "; ramMb = " + ramMb + "; hddGb = " + hddGb + "; freqCpuGhz = " + freqCpuGhz + "; cpuFsbMhz = " + cpuFsbMhz + "; cpuPackagePowerW = " + cpuPackagePowerW + "; thispacec = " + thispacec + ";+ namePc = " + namePc);
 
         //2.Расчет стоимости товара со скидкой
-        byte productX = 100;
-        short productY = 200;
+        byte priceX = 100;
+        short piceY = 200;
         float discount = 0.11f;
-        short sumXY = 300;
+        short totalSum = 300;
         System.out.println("2.Расчет стоимости товара со скидкой");
-        System.out.println("Итоговая сумма товаров со скидкой :");
-        System.out.println(sumXY - sumXY * discount);
-        System.out.println("Размер скидки :");
-        System.out.println(sumXY * discount);
+        System.out.println("Итоговая сумма товаров со скидкой :" + (totalSum - totalSum * discount));
+        System.out.println("Размер скидки :" + totalSum * discount);
 
         //3.Вывод на консоль слова JAVA
         System.out.println("3.Вывод на консоль слова JAVA");
@@ -32,40 +30,38 @@ public class VariablesTheme {
 
         //4.Отображение min и max значений числовых типов данных
         byte bMax = Byte.MAX_VALUE;
-        byte bMin = Byte.MIN_VALUE;
         short sMax = Short.MAX_VALUE;
-        short sMin = Short.MIN_VALUE;
         int intMax = Integer.MAX_VALUE;
-        int intMin = Integer.MIN_VALUE;
         long lMax = Long.MAX_VALUE;
-        Long lMin = Long.MIN_VALUE;
         float fMax = Float.MAX_VALUE;
-        float fMin = Float.MIN_VALUE;
         double dMax = Double.MAX_VALUE;
-        double dMin = Double.MIN_VALUE;
         System.out.println("4.Отображение min и max значений числовых типов данных");
         System.out.println("Отображение max значений числовых типов данных:");
         System.out.println("bMax = " + bMax + "; sMax = " + sMax + "; iMax = " + intMax + "; lMax = " + lMax + "; fMax = " + fMax + "; dMax = " + dMax);
-        System.out.println("Отображение min значений числовых типов данных:");
-        System.out.println("bMin = " + bMin + "; sMin = " + sMin + "; iMin = " + intMin + "; lMin = " + lMin + "; fMin = " + fMin + "; dMin = " + dMin);
-        System.out.println("Инкрементируемые значения for max:");
-        System.out.println("bMax = " + (bMax + 1) + "; sMax = " + (sMax + 1) + "; iMax = " + (intMax + 1) + "; lMax = " + (lMax + 1) + "; fMax = " + (fMax + 1) + "; dMax = " + (dMax + 1));
-        System.out.println("Декрементируемые значения for max:");
-        System.out.println("bMax = " + (bMax - 1) + "; sMax = " + (sMax - 1) + "; iMax = " + (intMax - 1) + "; lMax = " + (lMax - 1) + "; fMax = " + (fMax - 1) + "; dMax = " + (dMax - 1));
-        System.out.println("Инкрементируемые значения for min:");
-        System.out.println("bMin = " + (bMin + 1) + "; sMin = " + (sMin + 1) + "; iMin = " + (intMin + 1) + "; lMin = " + (lMin + 1) + "; fMin = " + (fMin + 1) + "; dMin = " + (dMin + 1));
-        System.out.println("Декрементируемые значения for min:");
-        System.out.println("bMin = " + (bMin - 1) + "; sMin = " + (sMin - 1) + "; iMin = " + (intMin - 1) + "; lMin = " + (lMin - 1) + "; fMin = " + (fMin - 1) + "; dMin = " + (dMin - 1));
+        bMax = ++bMax;
+        sMax = ++sMax;
+        intMax = ++intMax;
+        lMax = lMax;
+        fMax = ++fMax;
+        dMax = ++dMax;
+        System.out.println("Инкрементируемые значения:\n"+"bMax = " + bMax + "; sMax = " + sMax + "; iMax = " + intMax + "; lMax = " + lMax + "; fMax = " + fMax + "; dMax = " + dMax);
+        bMax = --bMax;
+        sMax = --sMax;
+        intMax = --intMax;
+        lMax = --lMax;
+        fMax = --fMax;
+        dMax = --dMax;
+        System.out.println("Декрементируемые значения:\n"+ "bMax = " + bMax + "; sMax = " + sMax + "; iMax = " + intMax + "; lMax = " + lMax + "; fMax = " + fMax + "; dMax = " + dMax);
 
         //5.Перестановка значений переменных
         System.out.println("5.Перестановка значений переменных");
-        float temperature = 36.6f;
-        double pi = 3.1416d;
-        System.out.println("temperature = " + temperature + "; pi = " + pi + ";");
-        double temp = pi;
-        pi = temperature;
-        temperature = (float)temp;
-        System.out.printf("temperature = " + temperature + "; pi = " + "%.1f",pi);
+        float number1 = 36.6f;
+        double number2 = 3.1416d;
+        System.out.println("number1 = " + number1 + "; number2 = " + number2 + ";");
+        double tempNumber = number2;
+        number2 = number1;
+        number1 = (float)tempNumber;
+        System.out.printf("number1 = " + number1 + "; number2 = " + "%.1f",number2);
         System.out.println(";");
 
         //6.Вывод символов и их кодов
@@ -73,48 +69,47 @@ public class VariablesTheme {
         byte ampersand = 38;
         byte at = 64;
         byte caret = 94;
-        byte underscoreerscore = 95;
+        byte underscore = 95;
         char chHash = (char) hash; 
         char chAmpersand = (char) ampersand;
         char chAt = (char) at;
         char chCaret = (char) caret;
-        char chunderscoreerscore = (char) underscoreerscore;
+        char chUnderscore = (char) underscore;
         System.out.println("6.Вывод символов и их кодов");
-        System.out.println(hash + " = " + chHash + " ; " + ampersand + " = " + chAmpersand + " ; " + at + " = " + chAt + " ; " + caret + " = " + chCaret + " ; " + underscoreerscore + " = " + chunderscoreerscore + " ; " );
+        System.out.println(hash + " = " + chHash + " ; " + ampersand + " = " + chAmpersand + " ; " + at + " = " + chAt + " ; " + caret + " = " + chCaret + " ; " + underscore + " = " + chUnderscore + " ; " );
 
         //7.Произведение и сумма цифр числа
-        int num = 345;
-        int digit = num % 10;
-        int interim = num / 10;
-        int ty = interim % 10;
-        int hundreds = interim / 10;
+        int srcNum = 345;
+        int digit = srcNum % 10;
+        int tmpNum = srcNum / 10;
+        int tens = tmpNum % 10;
+        int hundreds = tmpNum / 10;
         System.out.println("7.Произведение и сумма цифр числа");
-        System.out.println("Произведение : " + hundreds * ty * digit);
-        System.out.println("Сумма : " + (hundreds + ty + digit));
+        System.out.println("Произведение : " + hundreds * tens * digit);
+        System.out.println("Сумма : " + (hundreds + tens + digit));
 
         //8.Вывод на консоль ASCII-арт Дюка
         char space = ' ';
         char forwardSlash = '/';
-        char backslash = 92;
-        char leftParentheses = '(';
-        char rightParentheses = ')';
-        char underscore = '_';
+        char backslash = '\\';
+        char leftBracket = '(';
+        char rightBracket = ')';
         System.out.println("8.Вывод на консоль ASCII-арт Дюка");
         System.out.println(space + "" + space + "" + space + "" + space + "" + forwardSlash + "" + backslash + "" + space + "" + space + "" + space + "" + space + "" + space + "" + space);
         System.out.println(space + "" + space + "" + space + "" + forwardSlash + "" + space + "" + space + "" +backslash + "" + space + "" + space + "" + space + "" + space + "" + space);
-        System.out.println(space + "" + space + "" + forwardSlash + "" + underscore + "" + leftParentheses + "" + space + "" + rightParentheses + "" + backslash + "" + space + "" + space + "" + space + "" + space);
+        System.out.println(space + "" + space + "" + forwardSlash + "" + chUnderscore + "" + leftBracket + "" + space + "" + rightBracket + "" + backslash + "" + space + "" + space + "" + space + "" + space);
         System.out.println(space + "" + forwardSlash + "" + space + "" + space + "" + space + "" + space + "" + space + "" + space + "" + backslash + "" + space + "" + space + "" + space);
-        System.out.println(forwardSlash + "" + underscore + "" + underscore + "" + underscore + "" + underscore + "" + forwardSlash + "" + backslash + "" + underscore + "" + underscore + "" + backslash + "" + space + "" + space);
+        System.out.println(forwardSlash + "" + chUnderscore + "" + chUnderscore + "" + chUnderscore + "" + chUnderscore + "" + forwardSlash + "" + backslash + "" + chUnderscore + "" + chUnderscore + "" + backslash + "" + space + "" + space);
 
         //9.Отображение сотен, десятков, единиц числа
-        int numb = 123;
-        int interim1 = numb % 10;
-        int digit3 = numb % 10;
-        int interim2 = numb / 10;
-        int ty2 = interim2 % 10;
-        int hundreds1 = interim2 / 10;
+        int srcNumb = 123;
+        int tmpNumb2 = srcNumb % 10;
+        digit = srcNumb % 10;
+        tmpNum = srcNumb / 10;  
+        tens = tmpNum % 10;
+        hundreds = tmpNum / 10;
         System.out.println("9.Отображение сотен, десятков, единиц числа");
-        System.out.println(" сотни = " + hundreds1 + " ; десятки = " + ty2 + "; единицы числа = " + digit3);
+        System.out.println(" сотни = " + hundreds + " ; десятки = " + tens + "; единицы числа = " + digit);
 
         //10.Преобразование секунд
         int numberSec = 86399;
@@ -125,5 +120,5 @@ public class VariablesTheme {
         int min = (numberSec - secPerHour * hours) / secPerMinute;
         int sec = (numberSec - secPerHour * hours - secPerMinute * min);
         System.out.println(hours + ":" + min + ":" + sec);
-                }
+            }
 }
