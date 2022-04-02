e3c16eedcde5a9a62fb364eb3aeecb2c352b486e
Ilya547 <ilya547@bk.ru>
2022-04-02 14:05:28 +0300

    Load HW MyFirstGame

---
+++ Lesson 1/MyFirstGame.java
@@ -0,0 +1,34 @@
+public class MyFirstGame {
+    public static void main(String[] args) {
+        System.out.println("2.Игра “Угадай число”");
+        int srcNum = 100;
+        int srcNumPrev = 100;
+        int y = 99;
+        srcNum /= 2;
+        while ((srcNumPrev - srcNum) / 2 > 1) {
+            System.out.print(srcNum);
+             if (srcNum > y) {
+                System.out.print(" данное число больше того, что загадал компьютер\n");
+                srcNum = srcNum - (srcNumPrev - srcNum) / 2;
+                srcNumPrev /= 2;
+            } else if (srcNum < y) {
+                System.out.print(" данное число меньше того, что загадал компьютер\n");
+                srcNum = srcNum + (srcNumPrev - srcNum) / 2;
+            } else if (srcNum == y) {
+                break;
+            }
+        }
+        while (srcNum != y) {
+            System.out.print(srcNum);
+            if (srcNum > y) {
+                System.out.print(" @данное число больше того, что загадал компьютер\n");
+                srcNum -= 1; 
+            } else if (srcNum < y) {
+                System.out.print(" #данное число меньше того, что загадал компьютер\n");
+                srcNum += 1;
+            }
+        } 
+        System.out.print(srcNum);
+        System.out.print(" Вы победили!");
+    }   
+}
