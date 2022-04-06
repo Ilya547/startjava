0c3e31ad91e302455361b20bd3da99f536dbac3b
Ilya547 <ilya547@bk.ru>
2022-04-06 19:05:41 +0300

    load HW Wolf

---
+++ Lesson 2/Wolf.java
@@ -0,0 +1,29 @@
+public class Wolf {
+
+    String gender;
+    String nickname;
+    int weight;
+    int age;
+    String color;
+
+    public void walk() {
+        System.out.println("Идет");
+    }
+
+    public void sit() {
+        System.out.println("Cидит");
+    }
+
+    public void run() {
+        System.out.println("Бежит");
+    }
+
+    public void howl() {
+        System.out.println("Воет");
+    }
+
+    public void hunt() {
+        System.out.println("Охотится");
+    }
+
+}
