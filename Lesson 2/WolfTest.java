e456d453c1241db656588286b1f1b77cf534fdd3
Ilya547 <ilya547@bk.ru>
2022-04-07 16:28:29 +0300

    UPD HW WolfTest

--- Lesson 2/WolfTest.java
+++ Lesson 2/WolfTest.java
@@ -1,19 +1,20 @@
 public class WolfTest {
     public static void main(String[] args) {
         Wolf wolfOne = new Wolf();
-        wolfOne.gender = "man";
-        wolfOne.nickname = "Gocha";
-        wolfOne.weight = 59;
-        wolfOne.age = 3;
-        wolfOne.color = "Grey";
+        wolfOne.setGender("man");
+        wolfOne.setNickname("Gocha");
+        wolfOne.setWeight(59);
+        wolfOne.setAge(3);
+        wolfOne.setColor("Grey");
 
-        System.out.println("Мы создали волка по имени " + wolfOne.nickname + ", его цвет: " + wolfOne.color + ", а весит он: " + wolfOne.weight + " кг." );
+        System.out.println("Мы создали волка!" + "\nего имя: " + wolfOne.getNickname() + "\nего цвет: " + wolfOne.getColor() + "\nего вес: " + wolfOne.getWeight() + " кг" );
 
         wolfOne.walk();
         wolfOne.sit();
         wolfOne.run();
         wolfOne.howl();
         wolfOne.hunt();
+
 
     }
 }
