745430568b97fb5da2d46ec634720970e1c592f7
Ilya547 <ilya547@bk.ru>
2022-04-09 13:57:45 +0300

    load HW JaegerTest

---
+++ Lesson 2/JaegerTest.java
@@ -0,0 +1,34 @@
+public class JaegerTest {
+    public static void main(String[] args) {
+        Jaeger jaegerOne = new Jaeger();
+        jaegerOne.setModelName("Coyote Tango");
+        jaegerOne.setMark("Mark-1");
+        jaegerOne.setOrigin("Japan");
+        jaegerOne.setHeight(85.34f);
+        jaegerOne.setWeight(2.31f);
+        jaegerOne.setStrenght(7);
+        jaegerOne.setArmor(4);
+
+        System.out.println("Мы создали первого робота в классе : " + jaegerOne.getModelName());
+        System.out.println("Его сила равна :" + jaegerOne.getStrenght() + "; броня :" + jaegerOne.getArmor());
+        System.out.println("Он может выполнять общие действия:");
+        System.out.println(jaegerOne.scanKaiju());
+        jaegerOne.killKaiju();
+
+        Jaeger jaegerTwo = new Jaeger();
+        jaegerTwo.setModelName("Gipsy Danger");
+        jaegerTwo.setMark("Mark-3");
+        jaegerTwo.setOrigin("USA");
+        jaegerTwo.setHeight(79.25f);
+        jaegerTwo.setWeight(1.98f);
+        jaegerTwo.setStrenght(8);
+        jaegerTwo.setArmor(6);
+
+        System.out.println("Мы создали второго робота в классе : " + jaegerTwo.getModelName());
+        System.out.println("Его сила равна :" + jaegerTwo.getStrenght() + "; броня :" + jaegerTwo.getArmor());
+        System.out.println("Он может выполнять общие действия:");
+        System.out.println(jaegerTwo.scanKaiju());
+        jaegerTwo.killKaiju();
+
+    }
+}
