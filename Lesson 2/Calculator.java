56bccb661faae7c5a2efde5d8ebdcf97a341aa40
Ilya547 <ilya547@bk.ru>
2022-04-09 11:50:12 +0300

    load HW Calc v2

---
+++ Lesson 2/Calculator.java
@@ -0,0 +1,54 @@
+public class Calculator {
+        private int result;
+        private int firstNumber;
+        private int secondNumber;
+        private char sign;
+
+        public int getResult() {
+            return result;
+        }
+
+        public void setResult(int result) {
+            this.result = result;
+        }
+
+        public void setFirstNumber(int firstNumber) {
+            this.firstNumber = firstNumber;
+        }
+
+        public void setSecondNumber(int secondNumber) {
+            this.secondNumber = secondNumber;
+        }
+
+        public void setSign(char sign) {
+            this.sign = sign;
+        }
+
+        public void answer() {
+            int result = 1;
+            switch(sign) {
+                case '+' :
+                    result = firstNumber + secondNumber;
+                    break;
+                case '-' :
+                    result = firstNumber - secondNumber;
+                    break;
+                case '*' :
+                    result = firstNumber * secondNumber;
+                    break;
+                case '/' :
+                    result = firstNumber / secondNumber;
+                    break;
+                case '%' :
+                    result = firstNumber % secondNumber;
+                    break;
+                case '^' :
+                    for (int i = 0; i < secondNumber; i++) {
+                        result *= firstNumber;
+                    }
+                    //System.out.println(result);
+                    break;
+            }
+            System.out.println(result);
+        }
+}
