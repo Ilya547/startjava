50910ce6480ae19cf16748b48afe46f5b1bc01d3
unknown <ilya547@bk.ru>
2022-04-14 21:43:11 +0300

    load HW Player

---
+++ Lesson 2/Player.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Player {
+    private String name;
+    private int playerNumber;
+
+    public int getPlayerNumber() {
+        return playerNumber;
+    }
+
+    public void setPlayerNumber(int playerNumber) {
+        this.playerNumber = playerNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Player(String name) {
+        this.name = name;
+    }
+}
