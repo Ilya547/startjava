a5153f5504a84b703cf33beb415796f660ae7bab
unknown <ilya547@bk.ru>
2022-04-15 14:10:36 +0300

    UPD HW  CalculatorTest

--- Lesson 2/CalculatorTest.java
+++ Lesson 2/CalculatorTest.java
@@ -1,30 +1,27 @@
 import java.util.Scanner;
+
 public class CalculatorTest {
     public static void main(String[] args) {
-        Calculator calculatorOne = new Calculator();
+        Calculator calculator = new Calculator();
         Scanner sc = new Scanner(System.in);
         String reply = "yes";
         while (reply.equals("yes")) {
             System.out.println("Введите первое число:");
-
-            calculatorOne.setFirstNumber(sc.nextInt());
+            calculator.setFirstNumber(sc.nextInt());
 
             System.out.println("Введите знак математической операции:");
-
-            calculatorOne.setSign(sc.next().charAt(0));
+            calculator.setSign(sc.next().charAt(0));
 
             System.out.println("Введите второе число:");
-
-            calculatorOne.setSecondNumber(sc.nextInt());
+            calculator.setSecondNumber(sc.nextInt());
 
             System.out.println("Результат: ");
-            calculatorOne.calculations();
+            calculator.startCalculation();
             
-            reply = "";
-            while ((!reply.equals("yes")) && (!reply.equals("no"))) {
+            do {
                 reply = sc.nextLine();
                 System.out.println("Хотите продолжить вычисления? [yes/no]:");
-            }
+            } while ((!reply.equals("yes")) && (!reply.equals("no")));
         }
     }
 }
