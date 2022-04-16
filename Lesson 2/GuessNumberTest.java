6097982cd6c9301eccf4a916cd16a6c59647e407
unknown <ilya547@bk.ru>
2022-04-16 21:40:58 +0300

    UPD HW GuessNumberTest

--- Lesson 2/GuessNumberTest.java
+++ Lesson 2/GuessNumberTest.java
@@ -1,10 +1,7 @@
 import java.util.Scanner;
 
-import java.util.Random;
-
 public class GuessNumberTest {
     public static void main(String[] args) {
-
         Scanner scan = new Scanner(System.in);
 
         System.out.println("Введите имя первого игрока");
@@ -13,36 +10,14 @@ public class GuessNumberTest {
         System.out.println("Введите имя второго игрока");
         Player playerTwo = new Player(scan.next());
 
-        GuessNumber GuessNumberOne = new GuessNumber(playerOne.getName(), playerTwo.getName());
-
-        Random r = new Random();
-
-        GuessNumberOne.setTargetNumber(r.nextInt(101) + 1);
-
-        GuessNumberOne.setReply("yes");
-
-        while (GuessNumberOne.getReply().equals("yes")) {
-            System.out.println(playerOne.getName() + ", введите число");
-
-            GuessNumberOne.setnumberGame(scan.nextInt());
-
-            GuessNumberOne.runGame();
-
-            if (GuessNumberOne.getnumberGame() != GuessNumberOne.getTargetNumber()) {
-                System.out.println(playerTwo.getName() + ", введите число");
-
-                GuessNumberOne.setnumberGame(scan.nextInt());
-
-                GuessNumberOne.runGame();
-            }
-
-            while ((!GuessNumberOne.getReply().equals("yes")) && (!GuessNumberOne.getReply().equals("no"))) {
-                GuessNumberOne.setReply(scan.nextLine());
+        GuessNumber game = new GuessNumber(playerOne, playerTwo);
+        
+        do {
+            game.launch();
+            do {
                 System.out.println("Хотите продолжить игру? [yes/no]:");
-                if (GuessNumberOne.getReply().equals("yes")) {
-                    GuessNumberOne.setTargetNumber(r.nextInt(101) + 1);
-                } 
-            }
-        }
+                game.setReply(scan.nextLine());
+            } while ((!game.getReply().equals("yes")) && (!game.getReply().equals("no")));
+        } while (game.getReply().equals("yes"));
     }
 }
